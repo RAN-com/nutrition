d2c4a4c5c54830b7c37935a0915cb465c873bcae
Ebenezer <ebenezerrajan.dev@gmail.com>
2025-05-31 06:43:58 +0530

    fix: updated domain to vcard

--- src/renderer/src/constants/value.ts
+++ src/renderer/src/constants/value.ts
@@ -2,6 +2,6 @@
 // default is 3000
 
 const isDev = import.meta.env.NODE_ENV === 'development' || import.meta.env.DEV
-export const SERVER_URL = isDev ? 'http://nutrition.localhost:3000' : 'https://nutrition.raninfo.in'
+export const SERVER_URL = isDev ? 'http://vcard.localhost:3000' : 'https://vcard.raninfo.in'
 
-export const SERVER_DOMAIN = isDev ? 'nutrition.localhost:3000' : 'nutrition.raninfo.in'
+export const SERVER_DOMAIN = isDev ? 'vcard.localhost:3000' : 'vcard.raninfo.in'
